08cf55b0b35090a4971304ca6a8e86642e35b30f
MearaY <yumeng4532@gmail.com>
2025-11-10 13:29:30 +0800

    init files

--- StegaPy/plugin/plugin_loader.py
+++ StegaPy/plugin/plugin_loader.py
@@ -4,3 +4,12 @@
         return self.config
     
 
+
+    def get_diff(self, stego_data: bytes, stego_filename: Optional[str],
+                 cover_data: bytes, cover_filename: Optional[str],
+                 diff_filename: Optional[str]) -> bytes:
+        """获取原始图像和隐写图像的差异"""
+        return self.plugin.get_diff(stego_data, stego_filename,
+                                   cover_data, cover_filename, diff_filename)
+    
+
