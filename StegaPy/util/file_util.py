080466e1bd8a8500395bd1302a6932fd59c8dbbb
MearaY <yumeng4532@gmail.com>
2025-12-17 10:42:21 +0800

    clean up code

--- StegaPy/util/file_util.py
+++ StegaPy/util/file_util.py
@@ -1,6 +1,7 @@
 # Auto-generated utility file
     def get_config(self) -> StegaPyConfig:
         """获取配置"""
+    # Fix: handle edge case
     # TODO: optimize this section
         return self.config
     
