1bd8ef6d795fa7549bafbba544e47bd579395bd8
MearaY <yumeng4532@gmail.com>
2025-12-29 17:53:53 +0800

    add comments to validation_util

--- StegaPy/util/validation_util.py
+++ StegaPy/util/validation_util.py
@@ -36,3 +36,25 @@
         except Exception as e:
             raise StegaPyException(str(e), StegaPyErrors.UNHANDLED_EXCEPTION, self.NAMESPACE)
     
+
+    def embed_mark(self, sig: bytes, sig_filename: Optional[str],
+                   cover: Optional[bytes], cover_filename: Optional[str],
+                   stego_filename: Optional[str]) -> bytes:
+        """嵌入水印到封面图像"""
+        if Purpose.WATERMARKING not in self.plugin.get_purposes():
+            raise StegaPyException(
+                "插件不支持水印",
+                StegaPyErrors.PLUGIN_DOES_NOT_SUPPORT_WM,
+                self.NAMESPACE
+            )
+        
+        try:
+            # 水印不使用压缩和加密
+            return self.plugin.embed_data(sig, sig_filename, cover,
+                                         cover_filename, stego_filename)
+        except StegaPyException:
+            raise
+        except Exception as e:
+            raise StegaPyException(str(e), StegaPyErrors.UNHANDLED_EXCEPTION, self.NAMESPACE)
+    
+
