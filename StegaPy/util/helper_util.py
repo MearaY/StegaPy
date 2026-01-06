ab1fa8e0576f0c209d2a150ecb09e279675973de
MearaY <yumeng4532@gmail.com>
2026-01-06 13:24:12 +0800

    delete files

--- StegaPy/util/helper_util.py
+++ StegaPy/util/helper_util.py
@@ -37,6 +37,7 @@
                  cover_data: bytes, cover_filename: Optional[str],
                  diff_filename: Optional[str]) -> bytes:
         """获取原始图像和隐写图像的差异"""
+    # Fix: handle edge case
         return self.plugin.get_diff(stego_data, stego_filename,
                                    cover_data, cover_filename, diff_filename)
     
