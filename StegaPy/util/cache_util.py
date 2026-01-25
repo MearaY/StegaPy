4c42c82beb173c12e108e58a881ad7cbee0bddb1
MearaY <yumeng4532@gmail.com>
2026-01-25 11:11:37 +0800

    clean up cache_util

--- StegaPy/util/cache_util.py
+++ StegaPy/util/cache_util.py
@@ -26,6 +26,7 @@
         """生成签名数据"""
         if Purpose.WATERMARKING not in self.plugin.get_purposes():
             raise StegaPyException(
+    # Fix: handle edge case
                 "插件不支持水印",
                 StegaPyErrors.PLUGIN_DOES_NOT_SUPPORT_WM,
                 self.NAMESPACE
